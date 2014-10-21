da2853a23f3df5728ead4f06ea176db9aeba52bd
huydangnguyen <ndh290893@gmail.com>
2014-10-21 02:16:21 -0700

    c9

--- handle.js
+++ handle.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         back.height = vd.clientHeight;
         
         //vẽ từng frame hình lên canvas1 đồng thời thực hiện làm mờ trung bình
-        draw_and_blur(this, context1, backcontext,  canvas1.width, canvas1.height);
+        draw_and_extract_edge(this, context1, backcontext,  canvas1.width, canvas1.height);
         
         //vẽ từng frame hình lên canvas2 đồng thời thực hiện lấy biên cạnh
         //draw_and_extract_edge(this, context2, backcontext,  canvas2.width, canvas2.height);
@@ -29,8 +29,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-//vẽ từng frame hình lên canvas đồng thời thực hiện làm mờ trung bình
-function draw_and_blur(vd, context, backcontext, width, height)
+////vẽ từng frame hình lên canvas đồng thời thực hiện làm mờ trung bình
+//function draw_and_blur(vd, context, backcontext, width, height)
+//{
+//    if(vd.paused || vd.ended) return false;
+//    
+//    backcontext.drawImage(vd, 0, 0, width, height);
+//   
+//    var iData = backcontext.getImageData(0, 0, width, height);
+//    var pData = iData.data;
+//    var rData = iData.data;
+//   
+//    var step = iData.width*4;
+// 
+//    var i;
+//    var iPrev;
+//    var iNext;
+//    for (i = 0; i < pData.length; i++)
+//    {
+//        if(i%4 == 3)
+//        {
+//            continue;
+//        }       
+//        
+//        iPrev = i - step;
+//        iNext = i + step;
+//        rData[i] = (pData[iPrev-4] + pData[iPrev] + pData[iPrev+4]
+//                    + pData[i-4] + pData[i] + pData[i+4]
+//                    + pData[iNext-4] + pData[iNext] + pData[iNext+4])/9.0;
+//    }    
+//    
+//        
+//    iData.data = rData;
+//    context.putImageData(iData, 0, 0);
+//
+//    
+//    //Xử lý frame kế tiếp
+//    setTimeout(draw_and_blur, 10, vd, context, backcontext, width, height); 
+//}
+
+//vẽ từng frame hình lên canvas đồng thời thực hiện trích biên
+function draw_and_extract_edge(vd, context, backcontext, width, height)
 {
     if(vd.paused || vd.ended) return false;
     
@@ -40,11 +79,12 @@ function draw_and_blur(vd, context, backcontext, width, height)
     var pData = iData.data;
     var rData = iData.data;
    
-    var step = iData.width*4;
- 
+  
     var i;
     var iPrev;
     var iNext;
+    var dx, dy;
+    var step = iData.width*4;
     for (i = 0; i < pData.length; i++)
     {
         if(i%4 == 3)
@@ -54,9 +94,13 @@ function draw_and_blur(vd, context, backcontext, width, height)
         
         iPrev = i - step;
         iNext = i + step;
-        rData[i] = (pData[iPrev-4] + pData[iPrev] + pData[iPrev+4]
-                    + pData[i-4] + pData[i] + pData[i+4]
-                    + pData[iNext-4] + pData[iNext] + pData[iNext+4])/9.0;
+        dx = Math.abs(pData[iNext - 4] - pData[iPrev - 4]
+                      + 2*(pData[iNext] - pData[iPrev])
+                      + pData[iNext + 4] - pData[iPrev + 4]);
+        dy = Math.abs(pData[iPrev + 4] - pData[iPrev - 4]
+                      + pData[i + 4] - pData[i - 4]
+                      + pData[iNext + 4] - pData[iNext - 4]);
+        rData[i] = dx + dy;
     }    
     
         
@@ -64,8 +108,8 @@ function draw_and_blur(vd, context, backcontext, width, height)
     context.putImageData(iData, 0, 0);
 
     
-    //Repeat
-    setTimeout(draw_and_blur, 10, vd, context, backcontext, width, height); 
+    //Xử lý frame kế tiếp
+    setTimeout(draw_and_extract_edge, 0, vd, context, backcontext, width, height); 
 }
 
 ////Video processing-------------------------------------------------------------------------------------------------------------------
