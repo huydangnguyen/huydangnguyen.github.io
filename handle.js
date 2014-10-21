72471aef3467e87a77e78127538de0334cd31da8
huydangnguyen <ndh290893@gmail.com>
2014-10-21 03:48:44 -0700

    c28

--- handle.js
+++ handle.js
@@ -105,12 +105,19 @@ function draw_and_extract_edge(vd, context, backcontext, width, height)
         iPrev = i - step;
         iNext = i + step;
         //gray = pData[i]*0.3 + pData[i+1]*0.59 + pData[i+2]*0.11;
-        dx = Math.abs(((pData[iNext - 4] - pData[iPrev - 4])/2
-                      + (pData[iNext] - pData[iPrev])/2
-                      + (pData[iNext + 4] - pData[iPrev + 4])/2)/3,2);
-        dy = Math.pow(((pData[iPrev + 4] - pData[iPrev - 4])/2
-                      + (pData[i + 4] - pData[i - 4])/2
-                      + (pData[iNext + 4] - pData[iNext - 4])/2)/3,2);
+//        dx = Math.abs(((pData[iNext - 4] - pData[iPrev - 4])/2
+//                      + (pData[iNext] - pData[iPrev])/2
+//                      + (pData[iNext + 4] - pData[iPrev + 4])/2)/3,2);
+//        dy = Math.pow(((pData[iPrev + 4] - pData[iPrev - 4])/2
+//                      + (pData[i + 4] - pData[i - 4])/2
+//                      + (pData[iNext + 4] - pData[iNext - 4])/2)/3,2);
+         dx = Math.pow(pData[iNext - 4] - pData[iPrev - 4]
+                      + 2*(pData[iNext] - pData[iPrev])
+                      + pData[iNext + 4] - pData[iPrev + 4],2);
+        dy = Math.pow(pData[iPrev + 4] - pData[iPrev - 4]
+                      + 2*(pData[i + 4] - pData[i - 4])
+                      + pData[iNext + 4] - pData[iNext - 4],2);
+        
         rData[i] = Math.sqrt(dx + dy);
         if(rData[i] > max)
         {
